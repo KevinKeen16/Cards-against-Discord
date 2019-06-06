18f3e44b400ec04ded5f31a9af67966fc55822cd
Remco <hello@dougley.com>
2019-06-06 14:30:17 +0200

    add discord game sdk

---
+++ Assets/Scripts/AvatarImage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+using UnityEngine.UI;
+
+public class AvatarImage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        var discord = DiscordGameSDK.Instance.discord;
+        await DiscordGameSDK.Instance.SDKReady.Task;
+        User user = discord.GetUserManager().GetCurrentUser();
+        Debug.Log(user.Id);
+        var handle = new ImageHandle()
+        {
+            Type = ImageType.User,
+            Id = user.Id,
+            Size = 256
+        };
+        discord.GetImageManager().Fetch(handle, false, (result, rere) =>
+        {
+            Texture avatar = discord.GetImageManager().GetTexture(handle);
+            RawImage image = gameObject.GetComponent<RawImage>();
+            image.texture = avatar;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DiscordGameSDK.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DiscordGameSDK : MonoBehaviour
+{
+    public Discord.Discord discord = new Discord.Discord(553923478646947853, (uint)CreateFlags.Default);
+    public TaskCompletionSource<bool> SDKReady = new TaskCompletionSource<bool>();
+
+    private static DiscordGameSDK instance;
+
+    public static DiscordGameSDK Instance
+    {
+        get { return instance ?? (instance = new GameObject("DiscordGameSDK").AddComponent<DiscordGameSDK>()); }
+    }
+
+    private void Awake()
+    {
+        discord.SetLogHook(LogLevel.Debug, DebugLogging);
+        discord.GetUserManager().OnCurrentUserUpdate += OnCurrentUserUpdate;
+    }
+
+    private void OnCurrentUserUpdate()
+    {
+        Debug.Log("Discord SDK Ready!");
+
+        SDKReady.SetResult(true);
+    }
+
+    private void Update()
+    {
+        discord.RunCallbacks();
+    }
+
+    public void DebugLogging(LogLevel level, string message)
+    {
+        Debug.Log(message);
+    }
+
+    private void Start()
+    {
+        Debug.Log("TEST");
+    }
+}
